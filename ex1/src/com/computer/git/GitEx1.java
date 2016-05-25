21ff2142da88a9a949f0de717b298f75a1d72342
ytjin <ytjin@hanbat.ac.kr>
2016-05-25 16:01:57 +0900

    second branch test

--- ex1/src/com/computer/git/GitEx1.java
+++ ex1/src/com/computer/git/GitEx1.java
@@ -4,7 +4,10 @@ public class GitEx1 {
 
 	public static void main(String[] args) {
 		System.out.println(" Git is confusing");
-
+	Person p1 = new Person();
+	p1.setName("Fred");
+	p1.setMaximumBooks(3);
+	p1.toString();
 	}
 
 }
---
+++ ex1/src/com/computer/git/Person.java
@@ -0,0 +1,34 @@
+package com.computer.git;
+
+public class Person {
+	// fields
+		private String name;
+		private int maximumBooks;
+		
+	// constructors
+		public Person() {
+			name = "unknown name";
+			maximumBooks = 3;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String anyName) {
+			name = anyName;
+		}
+
+		public int getMaximumBooks() {
+			return maximumBooks;
+		}
+
+		public void setMaximumBooks(int maximumBooks) {
+			this.maximumBooks = maximumBooks;
+		}
+		
+		public String toString() {
+	        return this.getName() + "(" + this.getMaximumBooks() +
+	         " books)";
+	}
+
+}
